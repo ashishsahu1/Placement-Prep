88609dc10e8bf51216bce02f7d0ecd743aaa3095
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-05-30 08:24:19 +0530

    character array

--- CharacterArray/largestWordInSentences.cpp
+++ CharacterArray/largestWordInSentences.cpp
@@ -5,6 +5,43 @@ int main()
 {
     // code
     char chr[100];
-    cin.getline(chr,)
+    cin.getline(chr,100);
+    cin.ignore();
+    int largest=INT_MIN,i=0;
+    int c=0;
+    int currstrt=0,maxst;
+    // chr+=' ';
+    int n=0;
+    while(chr[i]!='\0'){
+        n++;
+        i++;
+    }
+    cout<<n<<endl;
+
+    i=0;
+    
+    while(1){
+        if(chr[i]==' ' || chr[i]=='\0'){ 
+            if(c>largest){
+                largest = c;
+                maxst = currstrt;
+            }
+            c=0;
+            currstrt=currstrt+1;
+        }
+        else{
+            currstrt+=1;
+            c++;
+        }
+            
+        if(chr[i]=='\0')
+            break;
+        i++; 
+    }
+
+    cout<<largest<<endl;
+    for(int i=0;i<n;i++){
+        cout<<chr[i+maxst];
+    }
     return 0;
 }
