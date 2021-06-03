9a9cb52c0b608eef1140ef1ccc3db72fa0ffa183
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-06-03 13:01:15 +0530

    4 bit function

--- BitManipulation/updateBit.cpp
+++ BitManipulation/updateBit.cpp
@@ -1,23 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int clearBit(int n,int p){
-    return (n&(~(1<<p)));
-}
+int updateBit(int n,int p,int v){
+    int mask = ~(1<<p);
+    n = n&mask;
 
-int setBit(int n,int p){
-    return (n | (1<<p));
+    mask = (1<<p); //p==v
+    return (n|mask);
+
 }
 
 int main()
 {
     int n,p;
-    // cin>>n;
-    // cin>>p;
-    n=5;
-    p=1;
+    cin>>n;
+    cin>>p;
+    // n=5;
+    // p=1;
+    int val;
+    cin>>val;
 
-    cout<<setBit(clearBit(n,p),p)<<endl;
+    cout<<updateBit(n,p,val)<<endl;
 
     return 0;
 }
