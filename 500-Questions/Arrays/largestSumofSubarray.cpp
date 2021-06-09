aee6ea063e4685a016911e989b07ce2168796deb
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-06-09 17:48:18 +0530

    largest sum subarray

--- 500-Questions/Arrays/largestSumofSubarray.cpp
+++ 500-Questions/Arrays/largestSumofSubarray.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int subarraySum(int arr[],int n){
-    
+//kadane's algorithm
+int subarraySumMethod2(int arr[],int n){
+    int maxSum = INT_MIN;
+    int currSum = 0;
+    for(int i=0;i<n;i++){
+        currSum+=arr[i];
+        if(currSum<0){
+            currSum=0;
+        }
+        maxSum = max(currSum,maxSum);
+    }
+    return maxSum;
 }
+
+//using iteration method
+int subarraySumMethod1(int arr[],int n){
+    int maxSum=INT_MIN;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int sum=0;
+            for(int k=i;k<=j;k++){
+                sum+=arr[k];
+            }
+            maxSum = max(sum,maxSum);
+            sum=0;
+        }
+        cout<<endl;
+    }
+    return maxSum;
+}
+
+//using Kadane algorithm
+
 
 int main()
 {
@@ -15,6 +45,8 @@ int main()
         cin>>arr[i];
     }
 
-    cout<<subarraySum(arr,n);
+    //method 1
+    cout<<subarraySumMethod1(arr,n)<<endl;
+    cout<<subarraySumMethod2(arr,n)<<endl;
     return 0;
 }
