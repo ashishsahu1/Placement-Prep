1a4f7d1899a6560aa130d330e285797ae8531eb6
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-06-30 22:36:21 +0530

    garbage

--- Recursion/GFG/StringProblem/1.cpp
+++ Recursion/GFG/StringProblem/1.cpp
@@ -3,8 +3,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPal(string str,int beg,int end){
+    while(beg<end){
+        if(str[beg]!=str[end])
+            return false;
+        beg++;
+        end--;
+    }
+    return true;
+}
+
+string printPalindrome(string st){
+    
+}
+
 int main()
 {
     // code
+    string st;
+    getline(cin,st);
+    printPalindrome(st);
     return 0;
 }
