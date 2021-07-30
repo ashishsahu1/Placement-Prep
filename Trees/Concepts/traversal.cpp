a78651d0849e5e4c41271b351e43b4ed72ec0112
Ashish Sahu <ashishsahu10428@gmail.com>
2021-07-30 15:51:45 +0530

    tree travresal

--- Trees/Concepts/traversal.cpp
+++ Trees/Concepts/traversal.cpp
@@ -1,93 +1,109 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
-    Node(int val){
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
         data = val;
         left = NULL;
         right = NULL;
     }
 };
 
-class TreeRec{
-    public:
+class TreeRec
+{
+public:
     // Node *root = NULL;
 
     //inorder using recursion
-    void inorderRec(Node * root){
-        if(root == NULL){
+    void inorderRec(Node *root)
+    {
+        if (root == NULL)
+        {
             return;
         }
         inorderRec(root->left);
-        cout<<root->data<<" ";
+        cout << root->data << " ";
         inorderRec(root->right);
     }
 
     //preorder using recursion
-    void preorderRec(Node* root){
-        if(root==NULL){
+    void preorderRec(Node *root)
+    {
+        if (root == NULL)
+        {
             return;
         }
-        cout<<root->data<<" ";
+        cout << root->data << " ";
         preorderRec(root->left);
         preorderRec(root->right);
     }
 
     //postorder using recursion
-    void postorderRec(Node *root){
-        if(root==NULL){
+    void postorderRec(Node *root)
+    {
+        if (root == NULL)
+        {
             return;
         }
         postorderRec(root->left);
         postorderRec(root->right);
-        cout<<root->data<<" ";
+        cout << root->data << " ";
     }
 
-
-    void levelOrder(queue<Node*> q){
-        if(q.empty()){
+    void levelOrder(queue<Node *> q)
+    {
+        if (q.empty())
+        {
             return;
         }
         q.push(q.front()->left);
         q.push(q.front()->right);
 
-        cout<<q.front()->data<<" ";
+        cout << q.front()->data << " ";
         q.pop();
 
         levelOrder(q);
     }
 };
 
-class TreeItt{ 
-    public:
-    void inorderItt(Node *root){
-        Node * curr = root;
-        stack<Node*> tempStack;
-        while(curr!=NULL || tempStack.empty()==false)
+class TreeItt
+{
+public:
+    void inorderItt(Node *root)
+    {
+        Node *curr = root;
+        stack<Node *> tempStack;
+        while (curr != NULL || tempStack.empty() == false)
         {
-            while(curr!=NULL){
+            while (curr != NULL)
+            {
                 tempStack.push(curr);
                 curr = curr->left;
             }
 
             curr = tempStack.top();
             tempStack.pop();
-            cout<<curr->data<<" ";
+            cout << curr->data << " ";
 
             curr = curr->right;
         }
     }
 
-    void preorderItt(Node* root){
-        Node * curr = root;
-        stack<Node*> tempStack;
-        while(curr!=NULL || tempStack.empty()==false){
-            while(curr!=NULL){
-                cout<<curr->data<<" ";
+    void preorderItt(Node *root)
+    {
+        Node *curr = root;
+        stack<Node *> tempStack;
+        while (curr != NULL || tempStack.empty() == false)
+        {
+            while (curr != NULL)
+            {
+                cout << curr->data << " ";
                 tempStack.push(curr);
                 curr = curr->left;
             }
@@ -107,7 +123,7 @@ int main()
              /  \    /  \
             40  50  60  70  
     */
-    
+
     //creation of tree
     Node *root = NULL;
     Node *n1 = new Node(10);
@@ -126,34 +142,34 @@ int main()
     n3->right = n7;
 
     /*  USING RECURSION  */
-    cout<<" ***  USING RECURSION  ***"<<endl;
+    cout << " ***  USING RECURSION  ***" << endl;
     TreeRec t1;
     //inorder
-    cout<<"Inorder : ";
+    cout << "Inorder : ";
     t1.inorderRec(root);
-    cout<<endl;
+    cout << endl;
 
     //preorder
-    cout<<"Postorder : ";
+    cout << "Postorder : ";
     t1.postorderRec(root);
-    cout<<endl;
+    cout << endl;
 
     //postorder
-    cout<<"Preorder : ";
+    cout << "Preorder : ";
     t1.preorderRec(root);
-    cout<<endl;
+    cout << endl;
 
     /*  Level order  */
-    queue<Node*> q;
+    queue<Node *> q;
     q.push(root);
-    cout<<"Level Order : ";
+    cout << "Level Order : ";
     t1.levelOrder(q);
-    cout<<endl;
+    cout << endl;
 
     /*  USING ITERATION AND STACK  */
-    cout<<" ***  USING ITERATION AND STACK  *** "<<endl;
+    cout << " ***  USING ITERATION AND STACK  *** " << endl;
     TreeItt t2;
-    cout<<"InOrder : ";
+    cout << "InOrder : ";
     t2.inorderItt(root);
 
     return 0;
