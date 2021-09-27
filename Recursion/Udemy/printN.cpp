1327d3c8acd51dd87b52ef1545d64630dd3e4600
Ashish Sahu <ashishsahu10428@gmail.com>
2021-09-27 22:51:40 +0530

    recursion

--- Recursion/Udemy/printN.cpp
+++ Recursion/Udemy/printN.cpp
@@ -2,15 +2,41 @@
 using namespace std;
 
 void printN(int n){
-    if(n==0){
+    if(n==6){
         return;
     }
     cout<<"Hello this is "<<n<<endl;
-    printN(n-1);
+    printN(n+1);
+}
+
+void printN2(int n,int i){
+
+    cout << "Hello this is " << i << endl;
+    //base condition
+    if(i==n){
+        return;
+    }
+
+    //recursion call
+    printN2(n,i+1);
+}
+
+void printN3(int n)
+{
+    //base condition
+    if (n==0)
+    {
+        return;
+    }
+
+    cout << "Hello this is " << n << endl;
+    //recursion call
+    printN3(n - 1);
+    cout << "hi this is " << n << endl;
 }
 
 int main(){
     int n;
     cin>>n;
-    printN(n);
+    printN3(n);
 }
